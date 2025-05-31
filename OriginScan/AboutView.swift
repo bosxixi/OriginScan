1db2ff02674171f9dfeaa5c105fa3f9f2b39171e
bosxixi <bosxixi@outlook.com>
2025-05-31 12:13:34 +1200

    Make support email clickable and draft email with app name and version in subject

--- OriginScan/AboutView.swift
+++ OriginScan/AboutView.swift
@@ -1,6 +1,9 @@
 import SwiftUI
+import MessageUI
 
 struct AboutView: View {
+    @State private var isMailComposerPresented = false
+
     var body: some View {
         VStack(spacing: 20) {
             Text(NSLocalizedString("about", comment: ""))
@@ -8,14 +11,45 @@ struct AboutView: View {
                 .fontWeight(.bold)
             Text(NSLocalizedString("publisher", comment: ""))
                 .font(.title2)
-            Text(NSLocalizedString("supportEmail", comment: ""))
-                .font(.title2)
+            Button(action: {
+                isMailComposerPresented = true
+            }) {
+                Text(NSLocalizedString("supportEmail", comment: ""))
+                    .font(.title2)
+                    .foregroundColor(.blue)
+                    .underline()
+            }
             Text(NSLocalizedString("appName", comment: ""))
                 .font(.title2)
             Text(NSLocalizedString("appVersion", comment: ""))
                 .font(.title2)
         }
         .padding()
+        .sheet(isPresented: $isMailComposerPresented) {
+            MailComposerView()
+        }
+    }
+}
+
+struct MailComposerView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> MFMailComposeViewController {
+        let mailComposer = MFMailComposeViewController()
+        mailComposer.setToRecipients(["support@scorpioplayer.com"])
+        mailComposer.setSubject("\(NSLocalizedString("appName", comment: "")) - \(NSLocalizedString("appVersion", comment: ""))")
+        mailComposer.mailComposeDelegate = context.coordinator
+        return mailComposer
+    }
+
+    func updateUIViewController(_ uiViewController: MFMailComposeViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
+    class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+        func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+            controller.dismiss(animated: true)
+        }
     }
 }
 
